f699fbac23a731f33e9f969b3efb56aed4bec77a
Alex Di Vito <alexdiv87@hotmail.com>
2017-06-12 17:00:15 -0700

    removed aws for SES

--- common/models/mail.js
+++ common/models/mail.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var async   = require('async');
-var aws     = require('aws-sdk');
-// Load your AWS credentials and try to instantiate the object.
-aws.config.update({region: 'us-west-2'});
-aws.config.loadFromPath(__dirname + '/../../server/configs/aws.json');
+// var aws     = require('aws-sdk');
+// // Load your AWS credentials and try to instantiate the object.
+// aws.config.update({region: 'us-west-2'});
+// aws.config.loadFromPath(__dirname + '/../../server/configs/aws.json');
 
-// Instantiate SES.
-var ses = new aws.SES();
+// // Instantiate SES.
+// var ses = new aws.SES();
 
 module.exports = function(Mail) {
     // after creating the databse record, send an email
@@ -65,122 +65,122 @@ module.exports = function(Mail) {
         });
     });
 
-    // Verify email addresses.
-    Mail.verify = function(email, cb) {
-        var params = {
-            EmailAddress: email
-        };
+    // // Verify email addresses.
+    // Mail.verify = function(email, cb) {
+    //     var params = {
+    //         EmailAddress: email
+    //     };
         
-        ses.verifyEmailAddress(params, function(err, data) {
-            if(err) {
-                return cb(err);
-            } 
-            else {
-                return cb(null, data);
-            } 
-        });
-    };
+    //     ses.verifyEmailAddress(params, function(err, data) {
+    //         if(err) {
+    //             return cb(err);
+    //         } 
+    //         else {
+    //             return cb(null, data);
+    //         } 
+    //     });
+    // };
 
-    // List verified email addresses.
-    Mail.listVerified = function(cb) {
-        ses.listVerifiedEmailAddresses(function(err, data) {
-            if(err) {
-                cb(err);
-            } 
-            else {
-                cb(null, data);
-            } 
-        });
-    };
+    // // List verified email addresses.
+    // Mail.listVerified = function(cb) {
+    //     ses.listVerifiedEmailAddresses(function(err, data) {
+    //         if(err) {
+    //             cb(err);
+    //         } 
+    //         else {
+    //             cb(null, data);
+    //         } 
+    //     });
+    // };
 
-    // Deleting verified email addresses.
-    Mail.deleteVerified = function(email, cb) {
-        var params = {
-            EmailAddress: email
-        };
+    // // Deleting verified email addresses.
+    // Mail.deleteVerified = function(email, cb) {
+    //     var params = {
+    //         EmailAddress: email
+    //     };
 
-        ses.deleteVerifiedEmailAddress(params, function(err, data) {
-            if(err) {
-                cb(err);
-            } 
-            else {
-                cb(null, data);
-            } 
-        });
-    };
+    //     ses.deleteVerifiedEmailAddress(params, function(err, data) {
+    //         if(err) {
+    //             cb(err);
+    //         } 
+    //         else {
+    //             cb(null, data);
+    //         } 
+    //     });
+    // };
 
-    // Sending RAW email including an attachment.
-    Mail.send = function(email, cb) {
-        var params = {
-            Destination: { /* required */
-                ToAddresses: [
-                    email
-                ]
-            },
-            Message: { /* required */
-                Body: { /* required */
-                    Html: {
-                        Data: '<p>Body Html</p>', /* required */
-                        Charset: 'UTF-8'
-                    },
-                    Text: {
-                        Data: 'Body text', /* required */
-                        Charset: 'UTF-8'
-                    }
-                },
-                Subject: { /* required */
-                    Data: 'Subject Value', /* required */
-                    Charset: 'UTF-8'
-                }
-            },
-            Source: 'alex.divito@mountainviewwebtech.ca', /* required */
-            ReplyToAddresses: [
-                'alex.divito@mountainviewwebtech.ca'
-            ],
-            SourceArn: 'arn:aws:ses:us-west-2:577142657045:identity/alex.divito@mountainviewwebtech.ca',
-            Tags: [
-                {
-                Name: 'MyName', /* required */
-                Value: 'MyVal' /* required */
-                },
-                /* more items */
-            ]
-        };
+    // // Sending RAW email including an attachment.
+    // Mail.send = function(email, cb) {
+    //     var params = {
+    //         Destination: { /* required */
+    //             ToAddresses: [
+    //                 email
+    //             ]
+    //         },
+    //         Message: { /* required */
+    //             Body: { /* required */
+    //                 Html: {
+    //                     Data: '<p>Body Html</p>', /* required */
+    //                     Charset: 'UTF-8'
+    //                 },
+    //                 Text: {
+    //                     Data: 'Body text', /* required */
+    //                     Charset: 'UTF-8'
+    //                 }
+    //             },
+    //             Subject: { /* required */
+    //                 Data: 'Subject Value', /* required */
+    //                 Charset: 'UTF-8'
+    //             }
+    //         },
+    //         Source: 'alex.divito@mountainviewwebtech.ca', /* required */
+    //         ReplyToAddresses: [
+    //             'alex.divito@mountainviewwebtech.ca'
+    //         ],
+    //         SourceArn: 'arn:aws:ses:us-west-2:577142657045:identity/alex.divito@mountainviewwebtech.ca',
+    //         Tags: [
+    //             {
+    //             Name: 'MyName', /* required */
+    //             Value: 'MyVal' /* required */
+    //             },
+    //             /* more items */
+    //         ]
+    //     };
     
-        ses.sendEmail(params, function(err, data) {
-            if (err) return cb(err);
-            else     return cb(null, data);
-        });
-    };
+    //     ses.sendEmail(params, function(err, data) {
+    //         if (err) return cb(err);
+    //         else     return cb(null, data);
+    //     });
+    // };
 
-    Mail.remoteMethod('verify', {
-        accepts: [
-            { arg: 'email', type: 'string', required: true }
-        ],
-        returns: { arg: 'data', type: 'object' },
-        http: { path: '/verify', verb: 'post' }
-    });
+    // Mail.remoteMethod('verify', {
+    //     accepts: [
+    //         { arg: 'email', type: 'string', required: true }
+    //     ],
+    //     returns: { arg: 'data', type: 'object' },
+    //     http: { path: '/verify', verb: 'post' }
+    // });
 
-    Mail.remoteMethod('listVerified', {
-        accepts: [
-        ],
-        returns: { arg: 'data', type: 'object' },
-        http: { path: '/listVerified', verb: 'post' }
-    });
+    // Mail.remoteMethod('listVerified', {
+    //     accepts: [
+    //     ],
+    //     returns: { arg: 'data', type: 'object' },
+    //     http: { path: '/listVerified', verb: 'post' }
+    // });
 
-    Mail.remoteMethod('deleteVerified', {
-        accepts: [
-            { arg: 'email', type: 'string', required: true }
-        ],
-        returns: { arg: 'data', type: 'object' },
-        http: { path: '/deleteVerified', verb: 'post' }
-    });
+    // Mail.remoteMethod('deleteVerified', {
+    //     accepts: [
+    //         { arg: 'email', type: 'string', required: true }
+    //     ],
+    //     returns: { arg: 'data', type: 'object' },
+    //     http: { path: '/deleteVerified', verb: 'post' }
+    // });
 
-    Mail.remoteMethod('send', {
-        accepts: [
-            { arg: 'email', type: 'string', required: true }
-        ],
-        returns: { arg: 'data', type: 'object' },
-        http: { path: '/send', verb: 'post' }
-    });
+    // Mail.remoteMethod('send', {
+    //     accepts: [
+    //         { arg: 'email', type: 'string', required: true }
+    //     ],
+    //     returns: { arg: 'data', type: 'object' },
+    //     http: { path: '/send', verb: 'post' }
+    // });
 };
