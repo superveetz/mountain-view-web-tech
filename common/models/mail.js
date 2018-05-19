1268e21e2450c59dd0386f079f5ce9c26fd1c0ea
superveetz <alexdiv87@hotmail.com>
2018-05-19 00:29:09 -0700

    ready for prod

--- common/models/mail.js
+++ common/models/mail.js
@@ -57,47 +57,14 @@ module.exports = function(Mail) {
             ReturnPath: "Mountain View Web Tech <alex@mountainviewwebtech.ca>",
             Source: "Mountain View Web Tech <alex@mountainviewwebtech.ca>"
         };
-        
-        var endpoint = "https://secretsmanager.us-west-2.amazonaws.com",
-            region = "us-west-2",
-            secretName = "prod/mountainviewwebtech/SES",
-            secret,
-            binarySecretData;
 
-        // Create a Secrets Manager client
-        var secretsmanager = new AWS.SecretsManager({
-            endpoint: endpoint,
-            region: region
-        });
-
-        secretsmanager.getSecretValue({SecretId: secretName}, function(err, data) {
-            if(err) {
-                if(err.code === 'ResourceNotFoundException')
-                    console.log("The requested secret " + secretName + " was not found");
-                else if(err.code === 'InvalidRequestException')
-                    console.log("The request was invalid due to: " + err.message);
-                else if(err.code === 'InvalidParameterException')
-                    console.log("The request had invalid params: " + err.message);
-            }
-            else {
-                // Decrypted secret using the associated KMS CMK
-                // Depending on whether the secret was a string or binary, one of these fields will be populated
-                if(data.SecretString !== "") {
-                    secret = data.SecretString;
-                } else {
-                    binarySecretData = data.SecretBinary;
-                }
-            }
-
-            var SES = new AWS.SES();
-        
-            // Your code goes here. 
-            SES.sendEmail(params, (err, data) => {
-                if (err) console.log(err, err.stack)
-                else console.log(data)
-                cb(err);
-            });
-            
+        var SES = new AWS.SES();
+    
+        // Your code goes here. 
+        SES.sendEmail(params, (err, data) => {
+            if (err) console.log(err, err.stack)
+            else console.log(data)
+            cb(err);
         });
         
     };
